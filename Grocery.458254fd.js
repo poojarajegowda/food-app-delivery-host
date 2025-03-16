500b5186df7fce9b0a9a1e0064d5dc53cd61e507
poojarajegowda <122740028+poojarajegowda@users.noreply.github.com>
2025-03-16 12:24:05 +0530

    updated dist

---
+++ Grocery.458254fd.js
@@ -0,0 +1,2 @@
+var e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire94c2;(0,e.register)("7kZKn",function(i,n){Object.defineProperty(i.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(i.exports,"default",{get:function(){return t},set:void 0,enumerable:!0,configurable:!0});var o=e("5aRSq"),t=()=>(0,o.jsx)("h1",{children:"This is the grocery detail. This contains the list of grocery items"})});
+//# sourceMappingURL=Grocery.458254fd.js.map
---
+++ Grocery.e4dd4749.js
@@ -0,0 +1,2 @@
+var e;(0,(e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire94c2).register)("dhpq2",function(i,n){Object.defineProperty(i.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(i.exports,"default",{get:function(){return t},set:void 0,enumerable:!0,configurable:!0});var o=e("cc6u1"),t=function(){return(0,o.jsx)("h1",{children:"This is the grocery detail. This contains the list of grocery items"})}});
+//# sourceMappingURL=Grocery.e4dd4749.js.map
